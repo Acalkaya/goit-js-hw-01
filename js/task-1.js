ece8ff912cafa1330cb90a81efae94fd75f5923e
Aysun Çalkaya <aysuncalkaya@gmail.com>
2024-11-05 21:55:37 +0300

    js update

--- js/task-1.js
+++ js/task-1.js
@@ -3,6 +3,10 @@ function makeTransaction(quantity, pricePerDroid) {
       `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
     );
   }
+  function makeTransaction(quantity, pricePerDroid) {
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
+    ;
+  }
   
   makeTransaction(5, 3000); // "You ordered 5 droids worth 15000 credits!"
   makeTransaction(3, 1000); // "You ordered 3 droids worth 3000 credits!"
--- js/task-2.js
+++ js/task-2.js
@@ -6,3 +6,4 @@ function getShippingMessage(country, price, deliveryFree) {
   getShippingMessage("Australia", 120, 50); // "Shipping to Australia will cost 170 credits"
   getShippingMessage("Germany", 80, 20); // "Shipping to Germany will cost 100 credits"
   getShippingMessage("Sweden", 100, 20); // "Shipping to Sweden will cost 120 credits"
+  
