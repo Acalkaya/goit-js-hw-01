35439b2c56fb9083c3ff7fc2e12fb55bab581b57
Aysun Çalkaya <aysuncalkaya@gmail.com>
2024-11-06 14:32:34 +0300

    js update

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,11 @@
 function makeTransaction(quantity, pricePerDroid) {
-    console.log(
-      `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
-    );
-  }
-  function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
-    ;
-  }
-  
-  makeTransaction(5, 3000); // "You ordered 5 droids worth 15000 credits!"
-  makeTransaction(3, 1000); // "You ordered 3 droids worth 3000 credits!"
-  makeTransaction(10, 500); // "You ordered 10 droids worth 5000 credits!"
+  return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
+
+//makeTransaction(5, 3000); // "You ordered 5 droids worth 15000 credits!"
+//makeTransaction(3, 1000); // "You ordered 3 droids worth 3000 credits!"
+//makeTransaction(10, 500); // "You ordered 10 droids worth 5000 credits!"
+
--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,8 @@
 function getElementWidth (content, padding, border) {    
-    console.log(parseInt(content) + 2*parseInt(padding) + 2*parseFloat(border))
+    return parseInt(content) + 2*parseFloat(padding) + 2*parseFloat(border)
 }
 
-getElementWidth("50px", "8px", "4px"); // 74
-getElementWidth("60px", "12px", "8.5px"); // 101
-getElementWidth("200px", "0px", "0px"); // 200
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.lod(getElementWidth("200px", "0px", "0px")); // 200
+
